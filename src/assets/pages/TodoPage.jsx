fdd0280e450e678d18bdbc5744f81e3d9b9de6a9
Dharssene-k-b <dharssenekb@gmail.com>
2025-12-19 15:57:08 +0530

    items added

--- src/assets/pages/TodoPage.jsx
+++ src/assets/pages/TodoPage.jsx
@@ -1,24 +1,79 @@
+// import { useState } from "react";
+
+// function TodoPage() {
+//     const [inputValue , setInputValue] = useState("");
+//     const [items , setItems] = useState([]);
+
+//     function handleAdd()
+//     {
+//         items.push(inputValue);
+//         setItems([...items]);
+//         setInputValue("");
+//     }
+//     return(
+//         <>
+//             <main>
+//                     <h1>Todo Page</h1>
+//                 <section>
+//                     <input type = "text" placeholder="Type ur work" value = {inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+//                     <button onClick = {handleAdd}>Add</button>
+//                 </section>
+//                 <section>
+//                     <ol>
+//                         {
+//                             items.map( (value , index) => 
+//                                 <li key = {index}>{value}</li>
+//                             )
+//                         }
+//                     </ol>
+//                 </section>
+//             </main>
+//         </>
+//     );
+// }
+//  export default TodoPage;
 import { useState } from "react";
 
 export default function()
 {
     const [inputValue , setInputValue] = useState("");
-    function handleInput(event) {
-        const value = event.target.value;
-        setInputValue(value);
+    const [items , setItems] = useState(["apple" , "orange"]);
+
+    function handleAdd() {
+        items.push(inputValue);
+        setItems([...items]);
+        setInputValue("");
     }
+    // function handleInput(event)
+    // {
+    //     const value = event.target.value;
+    //     setInputValue(value);
+    // }
     return(
         <>
-            <main>
+            <main className="font-mono h-screen w-screen flex flex-col justify-center items-center">
                 <h1>To Do Application</h1>
                 <section>
-                    <input value = {inputValue} onChange={handleInput} type="text"/>
-                    <button className="bg-red-500">add</button>
+                    <input 
+                        value = {inputValue} 
+                        onChange={(e) => setInputValue(e.target.value)} 
+                        type="text"
+                    />
+                    {/* <input 
+                        value = {inputValue} 
+                        onChange={handleInput} 
+                        type="text"
+                    /> */}
+                    <button className="bg-red-500" onClick={handleAdd}>add</button>
                 </section>
 
                 <section>
-                    <ol>
-                        <li>the input : {inputValue}</li>
+                    <ol className="list-decimal pl-5">
+                        {
+                            items.map((value,index) => {
+                                return <li key = {index}> {value}  </li>
+                            })
+                        }
                     </ol>
                 </section>
             </main>
